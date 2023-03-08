59d92d2ca443d7c5da5e4dd91426340d97e09caf
Ouassim <112521258+ouassim2@users.noreply.github.com>
2023-03-07 20:25:47 -0500

    done!

---
+++ 5kyu/kata.js
@@ -0,0 +1,28 @@
+// Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+Examples
+pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+pigIt('Hello world !');     // elloHay orldway !
+
+function pigIt(str){
+    let array = str.split(' ')
+    let word 
+    let result = []
+    
+     
+     array.forEach(e => {
+      word = e.split('') 
+       
+       if(word[0] !== '!' && word[0] !== '?'){
+         word.splice(word.length, 1, word[0] + "ay")
+         word.splice(word[0], 1)
+       }
+       
+      
+      result.push(word.join(''))
+       
+    })
+       
+   return result.join(' ')
+     
+    }
